811c55a4b2319cdf9ac223da9dd194971d5924aa
바카로이 <haeun8520@naver.com>
2025-11-18 16:47:03 +0900

    feat : 프로젝트 세팅

---
+++ src/main/java/org/delivery/voda/VodaApplication.java
@@ -0,0 +1,13 @@
+package org.delivery.voda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VodaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VodaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/delivery/voda/VodaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.delivery.voda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VodaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
